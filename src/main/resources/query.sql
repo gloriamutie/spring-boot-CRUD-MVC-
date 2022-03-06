05df1d3e281781e0a70f1cf6e4b752088ec46636
Gloria Mwongeli Mutie <gmutie@safaricom.co.ke>
2022-03-06 06:25:56 +0300

    new commit

--- src/main/resources/query.sql
+++ src/main/resources/query.sql
@@ -4,3 +4,5 @@ drop table if exists user_roles;
 create table role (id bigint not null auto_increment, description varchar(255), name varchar(255), primary key (id));
 create table user_roles (user_id bigint not null, role_id bigint not null, primary key (user_id, role_id));
 
+INSERT INTO role (id, description, name) VALUES (4, 'Admin role', 'ADMIN');
+INSERT INTO role (id, description, name) VALUES (5, 'User role', 'USER');
